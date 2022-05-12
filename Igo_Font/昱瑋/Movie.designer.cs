45be76c4591d64fe262a2bcb9a4154d68d11d472
jason79619@gmail.com <jason79619@gmail.com>
2022-05-12 16:54:27 +0800

    05/12 昱瑋

--- Igo_Font/昱瑋/Movie.designer.cs
+++ Igo_Font/昱瑋/Movie.designer.cs
@@ -59,7 +59,8 @@ namespace IGO_font
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label6.ForeColor = System.Drawing.SystemColors.Window;
-            this.label6.Location = new System.Drawing.Point(374, 85);
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.Location = new System.Drawing.Point(32, 52);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(71, 36);
             this.label6.TabIndex = 43;
@@ -70,49 +71,55 @@ namespace IGO_font
             this.btn_shop_car.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn_shop_car.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn_shop_car.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_shop_car.Location = new System.Drawing.Point(1090, 604);
+            this.btn_shop_car.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.btn_shop_car.Location = new System.Drawing.Point(658, 505);
             this.btn_shop_car.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_shop_car.Name = "btn_shop_car";
             this.btn_shop_car.Size = new System.Drawing.Size(271, 57);
             this.btn_shop_car.TabIndex = 42;
             this.btn_shop_car.Text = "加入購物車";
             this.btn_shop_car.UseVisualStyleBackColor = false;
+            this.btn_shop_car.Click += new System.EventHandler(this.btn_shop_car_Click);
             // 
             // btn_clear
             // 
             this.btn_clear.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btn_clear.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn_clear.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_clear.Location = new System.Drawing.Point(698, 612);
+            this.btn_clear.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.btn_clear.Location = new System.Drawing.Point(402, 508);
             this.btn_clear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_clear.Name = "btn_clear";
             this.btn_clear.Size = new System.Drawing.Size(164, 46);
             this.btn_clear.TabIndex = 41;
             this.btn_clear.Text = "清除購物車";
             this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
             // 
             // btn_delet
             // 
             this.btn_delet.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btn_delet.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn_delet.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_delet.Location = new System.Drawing.Point(498, 612);
+            this.btn_delet.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.btn_delet.Location = new System.Drawing.Point(229, 508);
             this.btn_delet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_delet.Name = "btn_delet";
             this.btn_delet.Size = new System.Drawing.Size(167, 46);
             this.btn_delet.TabIndex = 40;
             this.btn_delet.Text = "刪除該筆";
             this.btn_delet.UseVisualStyleBackColor = false;
+            this.btn_delet.Click += new System.EventHandler(this.btn_delet_Click);
             // 
             // dgv_shoppingcar
             // 
             this.dgv_shoppingcar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_shoppingcar.Location = new System.Drawing.Point(247, 671);
+            this.dgv_shoppingcar.Location = new System.Drawing.Point(29, 575);
             this.dgv_shoppingcar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgv_shoppingcar.Name = "dgv_shoppingcar";
             this.dgv_shoppingcar.RowHeadersWidth = 62;
             this.dgv_shoppingcar.RowTemplate.Height = 31;
-            this.dgv_shoppingcar.Size = new System.Drawing.Size(1284, 195);
+            this.dgv_shoppingcar.Size = new System.Drawing.Size(1029, 159);
             this.dgv_shoppingcar.TabIndex = 39;
             // 
             // btn_shop
@@ -120,20 +127,23 @@ namespace IGO_font
             this.btn_shop.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn_shop.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn_shop.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_shop.Location = new System.Drawing.Point(1181, 420);
+            this.btn_shop.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.btn_shop.Location = new System.Drawing.Point(805, 402);
             this.btn_shop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_shop.Name = "btn_shop";
             this.btn_shop.Size = new System.Drawing.Size(253, 58);
             this.btn_shop.TabIndex = 38;
             this.btn_shop.Text = "加入購物清單";
             this.btn_shop.UseVisualStyleBackColor = false;
+            this.btn_shop.Click += new System.EventHandler(this.btn_shop_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label5.ForeColor = System.Drawing.SystemColors.Window;
-            this.label5.Location = new System.Drawing.Point(263, 612);
+            this.label5.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.label5.Location = new System.Drawing.Point(30, 505);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(164, 46);
             this.label5.TabIndex = 37;
@@ -144,17 +154,19 @@ namespace IGO_font
             this.btn_time.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn_time.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn_time.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_time.Location = new System.Drawing.Point(1300, 151);
+            this.btn_time.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.btn_time.Location = new System.Drawing.Point(858, 130);
             this.btn_time.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_time.Name = "btn_time";
             this.btn_time.Size = new System.Drawing.Size(120, 51);
             this.btn_time.TabIndex = 36;
             this.btn_time.Text = "查詢";
             this.btn_time.UseVisualStyleBackColor = false;
+            this.btn_time.Click += new System.EventHandler(this.btn_time_Click);
             // 
             // cmb_Count
             // 
-            this.cmb_Count.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cmb_Count.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.cmb_Count.FormattingEnabled = true;
             this.cmb_Count.Items.AddRange(new object[] {
             "1",
@@ -167,10 +179,10 @@ namespace IGO_font
             "8",
             "9",
             "10"});
-            this.cmb_Count.Location = new System.Drawing.Point(666, 424);
+            this.cmb_Count.Location = new System.Drawing.Point(425, 407);
             this.cmb_Count.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmb_Count.Name = "cmb_Count";
-            this.cmb_Count.Size = new System.Drawing.Size(174, 43);
+            this.cmb_Count.Size = new System.Drawing.Size(174, 44);
             this.cmb_Count.TabIndex = 35;
             this.cmb_Count.Text = "請選擇數量";
             // 
@@ -179,7 +191,8 @@ namespace IGO_font
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label4.ForeColor = System.Drawing.SystemColors.Window;
-            this.label4.Location = new System.Drawing.Point(589, 434);
+            this.label4.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.label4.Location = new System.Drawing.Point(348, 410);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 36);
             this.label4.TabIndex = 34;
@@ -187,12 +200,12 @@ namespace IGO_font
             // 
             // cmb_Movie
             // 
-            this.cmb_Movie.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cmb_Movie.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.cmb_Movie.FormattingEnabled = true;
-            this.cmb_Movie.Location = new System.Drawing.Point(1090, 78);
+            this.cmb_Movie.Location = new System.Drawing.Point(595, 52);
             this.cmb_Movie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmb_Movie.Name = "cmb_Movie";
-            this.cmb_Movie.Size = new System.Drawing.Size(344, 43);
+            this.cmb_Movie.Size = new System.Drawing.Size(491, 44);
             this.cmb_Movie.TabIndex = 33;
             this.cmb_Movie.Text = "請選擇電影";
             // 
@@ -201,25 +214,27 @@ namespace IGO_font
             this.btn_seat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.btn_seat.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn_seat.ForeColor = System.Drawing.SystemColors.Window;
-            this.btn_seat.Location = new System.Drawing.Point(1040, 420);
+            this.btn_seat.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.btn_seat.Location = new System.Drawing.Point(658, 402);
             this.btn_seat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_seat.Name = "btn_seat";
             this.btn_seat.Size = new System.Drawing.Size(110, 58);
             this.btn_seat.TabIndex = 32;
             this.btn_seat.Text = "劃位";
             this.btn_seat.UseVisualStyleBackColor = false;
+            this.btn_seat.Click += new System.EventHandler(this.btn_seat_Click);
             // 
             // cmb_ticket
             // 
-            this.cmb_ticket.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cmb_ticket.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.cmb_ticket.FormattingEnabled = true;
             this.cmb_ticket.Items.AddRange(new object[] {
             "全票",
             "優待票"});
-            this.cmb_ticket.Location = new System.Drawing.Point(366, 427);
+            this.cmb_ticket.Location = new System.Drawing.Point(109, 402);
             this.cmb_ticket.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmb_ticket.Name = "cmb_ticket";
-            this.cmb_ticket.Size = new System.Drawing.Size(187, 43);
+            this.cmb_ticket.Size = new System.Drawing.Size(187, 44);
             this.cmb_ticket.TabIndex = 31;
             this.cmb_ticket.Text = "請選擇票種";
             // 
@@ -228,7 +243,8 @@ namespace IGO_font
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label3.ForeColor = System.Drawing.SystemColors.Window;
-            this.label3.Location = new System.Drawing.Point(265, 431);
+            this.label3.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.label3.Location = new System.Drawing.Point(32, 402);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(71, 36);
             this.label3.TabIndex = 30;
@@ -236,30 +252,31 @@ namespace IGO_font
             // 
             // cmb_MovieAdress
             // 
-            this.cmb_MovieAdress.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cmb_MovieAdress.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.cmb_MovieAdress.FormattingEnabled = true;
-            this.cmb_MovieAdress.Location = new System.Drawing.Point(498, 82);
+            this.cmb_MovieAdress.Location = new System.Drawing.Point(126, 52);
             this.cmb_MovieAdress.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmb_MovieAdress.Name = "cmb_MovieAdress";
-            this.cmb_MovieAdress.Size = new System.Drawing.Size(304, 43);
+            this.cmb_MovieAdress.Size = new System.Drawing.Size(304, 44);
             this.cmb_MovieAdress.TabIndex = 29;
             this.cmb_MovieAdress.Text = "請選擇影城";
+            this.cmb_MovieAdress.SelectedIndexChanged += new System.EventHandler(this.cmb_MovieAdress_SelectedIndexChanged);
             // 
             // DGV_Movie
             // 
             this.DGV_Movie.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_Movie.Location = new System.Drawing.Point(247, 218);
+            this.DGV_Movie.Location = new System.Drawing.Point(29, 215);
             this.DGV_Movie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DGV_Movie.Name = "DGV_Movie";
             this.DGV_Movie.RowHeadersWidth = 62;
             this.DGV_Movie.RowTemplate.Height = 31;
-            this.DGV_Movie.Size = new System.Drawing.Size(1284, 175);
+            this.DGV_Movie.Size = new System.Drawing.Size(1029, 158);
             this.DGV_Movie.TabIndex = 28;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.dateTimePicker2.Location = new System.Drawing.Point(979, 151);
+            this.dateTimePicker2.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(511, 130);
             this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(266, 45);
@@ -267,8 +284,8 @@ namespace IGO_font
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(566, 151);
+            this.dateTimePicker1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(191, 130);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(253, 45);
@@ -279,7 +296,8 @@ namespace IGO_font
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label2.ForeColor = System.Drawing.SystemColors.Window;
-            this.label2.Location = new System.Drawing.Point(360, 151);
+            this.label2.Image = global::Igo_Font.Properties.Resources.圖片38;
+            this.label2.Location = new System.Drawing.Point(32, 130);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(127, 36);
             this.label2.TabIndex = 25;
@@ -290,7 +308,8 @@ namespace IGO_font
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.ForeColor = System.Drawing.SystemColors.Window;
-            this.label1.Location = new System.Drawing.Point(996, 85);
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(505, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 36);
             this.label1.TabIndex = 24;
@@ -324,9 +343,11 @@ namespace IGO_font
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Movie";
             this.Text = "Movie";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.dgv_shoppingcar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Movie)).EndInit();
             this.ResumeLayout(false);
