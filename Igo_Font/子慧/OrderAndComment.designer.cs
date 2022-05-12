3be48eeab8869f93d67a89dc8576fa7eb50ac341
Joychang <joychang97@gmail.com>
2022-05-12 14:56:32 +0800

    joy_0512

--- Igo_Font/子慧/OrderAndComment.designer.cs
+++ Igo_Font/子慧/OrderAndComment.designer.cs
@@ -51,16 +51,20 @@ namespace IGO
             // 
             // Gb_Button
             // 
-            this.Gb_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Gb_Button.BackColor = System.Drawing.Color.Transparent;
+            this.Gb_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Gb_Button.Controls.Add(this.Rb_Month);
             this.Gb_Button.Controls.Add(this.Rb_3Month);
             this.Gb_Button.Controls.Add(this.Rb_CreditOrder);
             this.Gb_Button.Controls.Add(this.Rb_Cancel);
             this.Gb_Button.Controls.Add(this.Rb_All);
-            this.Gb_Button.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Gb_Button.Location = new System.Drawing.Point(68, 23);
+            this.Gb_Button.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Gb_Button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Gb_Button.Location = new System.Drawing.Point(51, 18);
+            this.Gb_Button.Margin = new System.Windows.Forms.Padding(2);
             this.Gb_Button.Name = "Gb_Button";
-            this.Gb_Button.Size = new System.Drawing.Size(954, 74);
+            this.Gb_Button.Padding = new System.Windows.Forms.Padding(2);
+            this.Gb_Button.Size = new System.Drawing.Size(716, 59);
             this.Gb_Button.TabIndex = 1;
             this.Gb_Button.TabStop = false;
             this.Gb_Button.Text = "訂單查詢";
@@ -68,10 +72,12 @@ namespace IGO
             // Rb_Month
             // 
             this.Rb_Month.AutoSize = true;
-            this.Rb_Month.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Rb_Month.Location = new System.Drawing.Point(172, 36);
+            this.Rb_Month.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Rb_Month.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Rb_Month.Location = new System.Drawing.Point(129, 29);
+            this.Rb_Month.Margin = new System.Windows.Forms.Padding(2);
             this.Rb_Month.Name = "Rb_Month";
-            this.Rb_Month.Size = new System.Drawing.Size(120, 23);
+            this.Rb_Month.Size = new System.Drawing.Size(104, 21);
             this.Rb_Month.TabIndex = 3;
             this.Rb_Month.TabStop = true;
             this.Rb_Month.Text = "一個月內訂單";
@@ -81,10 +87,12 @@ namespace IGO
             // Rb_3Month
             // 
             this.Rb_3Month.AutoSize = true;
-            this.Rb_3Month.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Rb_3Month.Location = new System.Drawing.Point(611, 36);
+            this.Rb_3Month.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Rb_3Month.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Rb_3Month.Location = new System.Drawing.Point(465, 29);
+            this.Rb_3Month.Margin = new System.Windows.Forms.Padding(2);
             this.Rb_3Month.Name = "Rb_3Month";
-            this.Rb_3Month.Size = new System.Drawing.Size(120, 23);
+            this.Rb_3Month.Size = new System.Drawing.Size(104, 21);
             this.Rb_3Month.TabIndex = 2;
             this.Rb_3Month.TabStop = true;
             this.Rb_3Month.Text = "三個月內訂單";
@@ -94,10 +102,12 @@ namespace IGO
             // Rb_CreditOrder
             // 
             this.Rb_CreditOrder.AutoSize = true;
-            this.Rb_CreditOrder.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Rb_CreditOrder.Location = new System.Drawing.Point(476, 36);
+            this.Rb_CreditOrder.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Rb_CreditOrder.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Rb_CreditOrder.Location = new System.Drawing.Point(357, 29);
+            this.Rb_CreditOrder.Margin = new System.Windows.Forms.Padding(2);
             this.Rb_CreditOrder.Name = "Rb_CreditOrder";
-            this.Rb_CreditOrder.Size = new System.Drawing.Size(120, 23);
+            this.Rb_CreditOrder.Size = new System.Drawing.Size(104, 21);
             this.Rb_CreditOrder.TabIndex = 2;
             this.Rb_CreditOrder.TabStop = true;
             this.Rb_CreditOrder.Text = "銀行轉帳訂單";
@@ -107,10 +117,12 @@ namespace IGO
             // Rb_Cancel
             // 
             this.Rb_Cancel.AutoSize = true;
-            this.Rb_Cancel.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Rb_Cancel.Location = new System.Drawing.Point(335, 36);
+            this.Rb_Cancel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Rb_Cancel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Rb_Cancel.Location = new System.Drawing.Point(251, 29);
+            this.Rb_Cancel.Margin = new System.Windows.Forms.Padding(2);
             this.Rb_Cancel.Name = "Rb_Cancel";
-            this.Rb_Cancel.Size = new System.Drawing.Size(105, 23);
+            this.Rb_Cancel.Size = new System.Drawing.Size(91, 21);
             this.Rb_Cancel.TabIndex = 1;
             this.Rb_Cancel.TabStop = true;
             this.Rb_Cancel.Text = "已取消訂單";
@@ -120,10 +132,12 @@ namespace IGO
             // Rb_All
             // 
             this.Rb_All.AutoSize = true;
-            this.Rb_All.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Rb_All.Location = new System.Drawing.Point(46, 36);
+            this.Rb_All.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Rb_All.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Rb_All.Location = new System.Drawing.Point(34, 29);
+            this.Rb_All.Margin = new System.Windows.Forms.Padding(2);
             this.Rb_All.Name = "Rb_All";
-            this.Rb_All.Size = new System.Drawing.Size(90, 23);
+            this.Rb_All.Size = new System.Drawing.Size(78, 21);
             this.Rb_All.TabIndex = 0;
             this.Rb_All.TabStop = true;
             this.Rb_All.Text = "全部訂單";
@@ -132,51 +146,60 @@ namespace IGO
             // 
             // Gb_AfterQuery
             // 
-            this.Gb_AfterQuery.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.Gb_AfterQuery.BackColor = System.Drawing.Color.Transparent;
+            this.Gb_AfterQuery.BackgroundImage = global::Igo_Font.Properties.Resources.圖片58;
+            this.Gb_AfterQuery.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Gb_AfterQuery.Controls.Add(this.DGV_OrderDetail);
-            this.Gb_AfterQuery.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Gb_AfterQuery.ForeColor = System.Drawing.Color.Black;
-            this.Gb_AfterQuery.Location = new System.Drawing.Point(68, 117);
+            this.Gb_AfterQuery.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Gb_AfterQuery.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Gb_AfterQuery.Location = new System.Drawing.Point(51, 81);
+            this.Gb_AfterQuery.Margin = new System.Windows.Forms.Padding(2);
             this.Gb_AfterQuery.Name = "Gb_AfterQuery";
-            this.Gb_AfterQuery.Size = new System.Drawing.Size(954, 282);
+            this.Gb_AfterQuery.Padding = new System.Windows.Forms.Padding(2);
+            this.Gb_AfterQuery.Size = new System.Drawing.Size(716, 245);
             this.Gb_AfterQuery.TabIndex = 2;
             this.Gb_AfterQuery.TabStop = false;
             this.Gb_AfterQuery.Text = "訂單查詢結果";
             // 
             // DGV_OrderDetail
             // 
+            this.DGV_OrderDetail.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
             this.DGV_OrderDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_OrderDetail.Location = new System.Drawing.Point(19, 26);
+            this.DGV_OrderDetail.Location = new System.Drawing.Point(76, 42);
+            this.DGV_OrderDetail.Margin = new System.Windows.Forms.Padding(2);
             this.DGV_OrderDetail.Name = "DGV_OrderDetail";
             this.DGV_OrderDetail.RowHeadersWidth = 51;
             this.DGV_OrderDetail.RowTemplate.Height = 27;
-            this.DGV_OrderDetail.Size = new System.Drawing.Size(918, 232);
+            this.DGV_OrderDetail.Size = new System.Drawing.Size(562, 149);
             this.DGV_OrderDetail.TabIndex = 0;
             // 
             // Tb_Comment
             // 
             this.Tb_Comment.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Tb_Comment.ForeColor = System.Drawing.Color.Black;
-            this.Tb_Comment.Location = new System.Drawing.Point(68, 467);
+            this.Tb_Comment.Location = new System.Drawing.Point(51, 374);
+            this.Tb_Comment.Margin = new System.Windows.Forms.Padding(2);
             this.Tb_Comment.Multiline = true;
             this.Tb_Comment.Name = "Tb_Comment";
-            this.Tb_Comment.Size = new System.Drawing.Size(969, 111);
+            this.Tb_Comment.Size = new System.Drawing.Size(728, 129);
             this.Tb_Comment.TabIndex = 3;
             // 
             // Lb_CommentProduct
             // 
             this.Lb_CommentProduct.AutoSize = true;
-            this.Lb_CommentProduct.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Lb_CommentProduct.ForeColor = System.Drawing.Color.Black;
-            this.Lb_CommentProduct.Location = new System.Drawing.Point(74, 408);
+            this.Lb_CommentProduct.BackColor = System.Drawing.Color.Transparent;
+            this.Lb_CommentProduct.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Lb_CommentProduct.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Lb_CommentProduct.Location = new System.Drawing.Point(56, 326);
+            this.Lb_CommentProduct.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Lb_CommentProduct.Name = "Lb_CommentProduct";
-            this.Lb_CommentProduct.Size = new System.Drawing.Size(43, 19);
+            this.Lb_CommentProduct.Size = new System.Drawing.Size(46, 21);
             this.Lb_CommentProduct.TabIndex = 5;
             this.Lb_CommentProduct.Text = "產品:";
             // 
             // Cb_Star
             // 
-            this.Cb_Star.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Cb_Star.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Cb_Star.ForeColor = System.Drawing.Color.Black;
             this.Cb_Star.FormattingEnabled = true;
             this.Cb_Star.Items.AddRange(new object[] {
@@ -185,29 +208,35 @@ namespace IGO
             "3",
             "2",
             "1"});
-            this.Cb_Star.Location = new System.Drawing.Point(583, 405);
+            this.Cb_Star.Location = new System.Drawing.Point(544, 336);
+            this.Cb_Star.Margin = new System.Windows.Forms.Padding(2);
             this.Cb_Star.Name = "Cb_Star";
-            this.Cb_Star.Size = new System.Drawing.Size(133, 27);
+            this.Cb_Star.Size = new System.Drawing.Size(124, 29);
             this.Cb_Star.TabIndex = 7;
             // 
             // Lb_5Star
             // 
             this.Lb_5Star.AutoSize = true;
-            this.Lb_5Star.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Lb_5Star.ForeColor = System.Drawing.Color.Black;
-            this.Lb_5Star.Location = new System.Drawing.Point(474, 413);
+            this.Lb_5Star.BackColor = System.Drawing.Color.Transparent;
+            this.Lb_5Star.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Lb_5Star.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Lb_5Star.Location = new System.Drawing.Point(411, 335);
+            this.Lb_5Star.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Lb_5Star.Name = "Lb_5Star";
-            this.Lb_5Star.Size = new System.Drawing.Size(103, 19);
+            this.Lb_5Star.Size = new System.Drawing.Size(129, 24);
             this.Lb_5Star.TabIndex = 8;
             this.Lb_5Star.Text = "產品評分輸入:";
             // 
             // Btn_Submit
             // 
-            this.Btn_Submit.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Btn_Submit.ForeColor = System.Drawing.Color.Black;
-            this.Btn_Submit.Location = new System.Drawing.Point(896, 422);
+            this.Btn_Submit.BackgroundImage = global::Igo_Font.Properties.Resources.圖片36;
+            this.Btn_Submit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Btn_Submit.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Btn_Submit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Btn_Submit.Location = new System.Drawing.Point(673, 330);
+            this.Btn_Submit.Margin = new System.Windows.Forms.Padding(2);
             this.Btn_Submit.Name = "Btn_Submit";
-            this.Btn_Submit.Size = new System.Drawing.Size(126, 34);
+            this.Btn_Submit.Size = new System.Drawing.Size(94, 39);
             this.Btn_Submit.TabIndex = 9;
             this.Btn_Submit.Text = "提交";
             this.Btn_Submit.UseVisualStyleBackColor = true;
@@ -216,28 +245,33 @@ namespace IGO
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(74, 437);
+            this.label1.Location = new System.Drawing.Point(56, 350);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(170, 19);
+            this.label1.Size = new System.Drawing.Size(149, 17);
             this.label1.TabIndex = 11;
             this.label1.Text = "產品評論輸入:(限250字)";
             // 
             // Cb_Product
             // 
+            this.Cb_Product.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Cb_Product.FormattingEnabled = true;
-            this.Cb_Product.Location = new System.Drawing.Point(192, 404);
+            this.Cb_Product.Location = new System.Drawing.Point(275, 335);
+            this.Cb_Product.Margin = new System.Windows.Forms.Padding(2);
             this.Cb_Product.Name = "Cb_Product";
-            this.Cb_Product.Size = new System.Drawing.Size(175, 23);
+            this.Cb_Product.Size = new System.Drawing.Size(132, 28);
             this.Cb_Product.TabIndex = 12;
             // 
             // OrderAndComment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(1140, 643);
+            this.BackgroundImage = global::Igo_Font.Properties.Resources.圖片38;
+            this.ClientSize = new System.Drawing.Size(855, 514);
             this.Controls.Add(this.Cb_Product);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Btn_Submit);
@@ -247,6 +281,7 @@ namespace IGO
             this.Controls.Add(this.Tb_Comment);
             this.Controls.Add(this.Gb_AfterQuery);
             this.Controls.Add(this.Gb_Button);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "OrderAndComment";
             this.Text = "OrderAndComment";
             this.Load += new System.EventHandler(this.OrderAndComment_Load);
